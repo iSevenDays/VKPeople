5744043d6f8d83dbf6b3cbf4d798fc17b4bb8451
Anton Sokolchenko <wsevendays@gmail.com>
2014-09-29 15:59:13 +0300

    QuickLookController added
    quick look still not working properly

---
+++ VKPeople/SKQuickLookController.h
@@ -0,0 +1,22 @@
+//
+//  SKQuickLookDataSource.h
+//  VKPeople
+//
+//  Created by Anton Sokolchenko on 9/11/14.
+//  Copyright (c) 2014 AntonSokolchenko. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <Quartz/Quartz.h>
+#import <QuickLook/QuickLook.h>
+
+@interface SKQuickLookController : NSResponder<QLPreviewPanelDataSource, QLPreviewPanelDelegate>
+{
+    NSMutableArray *pictures;
+}
+- (instancetype)init __attribute__((unavailable("use initWithIKImageBrowserView:")));
+
+- (instancetype)initWithIKImageBrowserView:(IKImageBrowserView *)imBrowserView;
+- (void)setPicturesURLs:(NSArray *)picturesURLs;
+
+@end
---
+++ VKPeople/SKQuickLookController.m
@@ -0,0 +1,75 @@
+//
+//  SKQuickLookDataSource.m
+//  VKPeople
+//
+//  Created by Anton Sokolchenko on 9/11/14.
+//  Copyright (c) 2014 AntonSokolchenko. All rights reserved.
+//
+
+#import "SKQuickLookController.h"
+#import "SKAppDelegate.h"
+#import "SKCollectionViewController.h"
+
+@implementation SKQuickLookController
+{
+    IKImageBrowserView *_imBrowserView;
+    QLPreviewPanel *_panel;
+}
+
+- (instancetype)initWithIKImageBrowserView:(IKImageBrowserView *)imBrowserView{
+    self = [super init];
+    if( self ){
+        pictures = [NSMutableArray array];
+        _imBrowserView = imBrowserView;
+    }
+    return self;
+}
+
+- (BOOL)acceptsPreviewPanelControl:(QLPreviewPanel *)panel{
+    return YES;
+}
+
+- (void)beginPreviewPanelControl:(QLPreviewPanel *)panel{
+    // This document is now responsible of the preview panel
+    // It is allowed to set the delegate, data source and refresh panel.
+    _panel = panel;
+    panel.delegate = self;
+    panel.dataSource = self;
+    
+    NSLog(@"We can now receive QL Events.");
+}
+
+- (void)endPreviewPanelControl:(QLPreviewPanel *)panel{
+    // This document loses its responsisibility on the preview panel
+    // Until the next call to -beginPreviewPanelControl: it must not
+    // change the panel's delegate, data source or refresh it.
+    [self resignFirstResponder];
+    _panel.delegate = nil;
+    _panel.dataSource = nil;
+    _panel = nil;
+    
+    NSLog(@"endPreviewPanelControl");
+    AntiARCRelease(self);
+}
+
+- (void)setPicturesURLs:(NSArray *)picturesURLs{
+    [pictures removeAllObjects];
+    pictures = [NSMutableArray arrayWithArray:picturesURLs];
+}
+
+- (id<QLPreviewItem>)previewPanel:(QLPreviewPanel *)panel previewItemAtIndex:(NSInteger)index{
+    return [NSURL URLWithString:[pictures objectAtIndex:index]];
+}
+
+- (NSRect)previewPanel:(QLPreviewPanel *)panel sourceFrameOnScreenForPreviewItem:(id <QLPreviewItem>)item{
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+    return [_imBrowserView convertRectToBase:[_imBrowserView visibleRect]];
+#pragma clang diagnostic pop
+}
+
+- (NSInteger)numberOfPreviewItemsInPreviewPanel:(QLPreviewPanel *)panel{
+    return pictures.count;
+}
+
+@end
